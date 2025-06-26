8d9d4c1ededfe6bec030a939e32c8ed3ec5a0371
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-06-26 11:49:22 +0700

    methode inorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -93,3 +93,18 @@ class BinaryTree
                 currentNode = currentNode->rightchild;
         }
     }
+
+    void inorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;;
+            return;
+    }
+    if (ptr == nullptr)
+    return
+
+    inorder(ptr->leftchild);
+    cout << ptr->info << " "; //parent
+    inorder(ptr->rightchild);
+}
